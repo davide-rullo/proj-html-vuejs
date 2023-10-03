97ea0ebbb91c46db6bf2ed791843c1c88244417a
davide-rullo <davide.rullo01@gmail.com>
2023-10-03 17:06:52 +0200

    add specialist section

--- src/components/AppSpecialist.vue
+++ src/components/AppSpecialist.vue
@@ -5,14 +5,128 @@ export default {
 </script>
 <template>
     <section class="specialist">
+        <div class="container">
+            <div class="specialist-row d-flex flex-column justify-content-center align-items-center">
+                <p>Specialists In Modern Construction</p>
+            </div>
+            <div class="hrow">
+            </div>
+            <div class="specialist-description text-center pt-4 pb-5">
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto consectetur quis corrupti quas.
+                    Illo, magnam?</p>
+            </div>
+            <div class="row row-cols-1 row-cols-sm-2 row-cols-md-4">
+                <div class="col p-2">
+                    <div class="card spec-card d-flex flex-column align-items-center justify-content-center">
+                        <div class="circle d-flex align-items-center justify-content-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor"
+                                class="bi bi-building" viewBox="0 0 16 16">
+                                <path
+                                    d="M4 2.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3 0a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1ZM4 5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM7.5 5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1ZM4.5 8a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Zm2.5.5a.5.5 0 0 1 .5-.5h1a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5h-1a.5.5 0 0 1-.5-.5v-1Zm3.5-.5a.5.5 0 0 0-.5.5v1a.5.5 0 0 0 .5.5h1a.5.5 0 0 0 .5-.5v-1a.5.5 0 0 0-.5-.5h-1Z" />
+                                <path
+                                    d="M2 1a1 1 0 0 1 1-1h10a1 1 0 0 1 1 1v14a1 1 0 0 1-1 1H3a1 1 0 0 1-1-1V1Zm11 0H3v14h3v-2.5a.5.5 0 0 1 .5-.5h3a.5.5 0 0 1 .5.5V15h3V1Z" />
+                            </svg>
 
+                        </div>
+                        <h6>Buildings</h6>
+                        <p class="card-describe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto
+                            consectetur quis corrupti quas. Illo, magnam?</p>
+                    </div>
+                </div>
+                <div class="col p-2">
+                    <div class="card spec-card d-flex flex-column align-items-center justify-content-center">
+                        <div class="circle d-flex align-items-center justify-content-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M0 224c0 17.7 14.3 32 32 32s32-14.3 32-32c0-53 43-96 96-96H320v32c0 12.9 7.8 24.6 19.8 29.6s25.7 2.2 34.9-6.9l64-64c12.5-12.5 12.5-32.8 0-45.3l-64-64c-9.2-9.2-22.9-11.9-34.9-6.9S320 19.1 320 32V64H160C71.6 64 0 135.6 0 224zm512 64c0-17.7-14.3-32-32-32s-32 14.3-32 32c0 53-43 96-96 96H192V352c0-12.9-7.8-24.6-19.8-29.6s-25.7-2.2-34.9 6.9l-64 64c-12.5 12.5-12.5 32.8 0 45.3l64 64c9.2 9.2 22.9 11.9 34.9 6.9s19.8-16.6 19.8-29.6V448H352c88.4 0 160-71.6 160-160z" />
+                            </svg>
+                        </div>
+                        <h6>Renovate</h6>
+                        <p class="card-describe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto
+                            consectetur quis corrupti quas. Illo, magnam?</p>
+                    </div>
+                </div>
+                <div class="col p-2">
+                    <div class="card spec-card d-flex flex-column align-items-center justify-content-center">
+                        <div class="circle d-flex align-items-center justify-content-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 576 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M575.8 255.5c0 18-15 32.1-32 32.1h-32l.7 160.2c0 2.7-.2 5.4-.5 8.1V472c0 22.1-17.9 40-40 40H456c-1.1 0-2.2 0-3.3-.1c-1.4 .1-2.8 .1-4.2 .1H416 392c-22.1 0-40-17.9-40-40V448 384c0-17.7-14.3-32-32-32H256c-17.7 0-32 14.3-32 32v64 24c0 22.1-17.9 40-40 40H160 128.1c-1.5 0-3-.1-4.5-.2c-1.2 .1-2.4 .2-3.6 .2H104c-22.1 0-40-17.9-40-40V360c0-.9 0-1.9 .1-2.8V287.6H32c-18 0-32-14-32-32.1c0-9 3-17 10-24L266.4 8c7-7 15-8 22-8s15 2 21 7L564.8 231.5c8 7 12 15 11 24z" />
+                            </svg>
+                        </div>
+                        <h6>Construct</h6>
+                        <p class="card-describe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto
+                            consectetur quis corrupti quas. Illo, magnam?</p>
+                    </div>
+                </div>
+                <div class="col p-2">
+                    <div class="card spec-card d-flex flex-column align-items-center justify-content-center">
+                        <div class="circle d-flex align-items-center justify-content-center mb-2">
+                            <svg xmlns="http://www.w3.org/2000/svg" height="1em"
+                                viewBox="0 0 640 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                                <path
+                                    d="M48 0C21.5 0 0 21.5 0 48V368c0 26.5 21.5 48 48 48H64c0 53 43 96 96 96s96-43 96-96H384c0 53 43 96 96 96s96-43 96-96h32c17.7 0 32-14.3 32-32s-14.3-32-32-32V288 256 237.3c0-17-6.7-33.3-18.7-45.3L512 114.7c-12-12-28.3-18.7-45.3-18.7H416V48c0-26.5-21.5-48-48-48H48zM416 160h50.7L544 237.3V256H416V160zM112 416a48 48 0 1 1 96 0 48 48 0 1 1 -96 0zm368-48a48 48 0 1 1 0 96 48 48 0 1 1 0-96z" />
+                            </svg>
+                        </div>
+                        <h6>Exclusive</h6>
+                        <p class="card-describe">Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe architecto
+                            consectetur quis corrupti quas. Illo, magnam?</p>
+                    </div>
+                </div>
+
+            </div>
+        </div>
     </section>
 </template>
 
 
 <style lang="scss" scoped>
+@use '../assets/scss/partials/variables.scss' as *;
+
 .specialist {
 
-    height: 200px;
+    height: 600px;
+}
+
+.specialist-row {
+    padding-top: 5rem;
+
+    p {
+        font-size: 1.5rem;
+    }
+}
+
+.hrow {
+    width: 15vw;
+    margin: auto;
+    height: 1px;
+    background-color: $avd-primary ;
+}
+
+.specialist-description p {
+    font-size: .9rem;
+}
+
+.spec-card {
+    background-color: #F5F5F5;
+    padding: 1rem;
+}
+
+.circle {
+    border: 1px solid grey;
+    width: 40px;
+    aspect-ratio: 1/1;
+    border-radius: 100%;
+}
+
+svg {
+    fill: grey;
+}
+
+.card-describe {
+    font-size: .7rem;
+    text-align: center;
 }
 </style>
