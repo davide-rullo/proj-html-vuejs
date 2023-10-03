2977b261105ab8196e97af75bfe0cda8bd924195
davide-rullo <davide.rullo01@gmail.com>
2023-10-03 12:08:30 +0200

    add header

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,17 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+
 export default {
-
   name: 'App',
-
   data() {
-    return {
-
-    }
-  }
+    return {};
+  },
+  components: { AppHeader }
 }
 </script>
 
 <template>
-  <nav class="navbar navbar-expand-lg bg-body-tertiary">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="#">
-
-      </a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
-        aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
-        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Home</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Link</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
-          </li>
-        </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-          <button class="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav>
+  <AppHeader></AppHeader>
 </template>
 
 <style lang="scss">
@@ -47,5 +19,9 @@ export default {
 
 body {
   font-family: 'Montserrat', sans-serif;
+}
+
+img {
+  max-width: 100%;
 }
 </style>
---
+++ src/components/appHeader.vue
@@ -0,0 +1,77 @@
+<script>
+export default {
+
+}
+</script>
+<template>
+    <header>
+        <nav class="navbar navbar-expand-lg ">
+            <div class="container-fluid header-wrapper">
+                <a class="navbar-brand" href="#">
+                    <img src="../assets/img/construction_logo.png" alt="Avada Construction">
+                </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02"
+                    aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse justify-content-end" id="navbarTogglerDemo02">
+                    <ul class="navbar-nav mb-2 mb-lg-0 gap-4">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">About</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Services</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Work</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Articles</a>
+                        </li>
+                        <div class="yellow-btn">
+                            <button>GET QUOTE</button>
+                        </div>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </header>
+</template>
+
+
+<style lang="scss" scoped>
+@use "../assets/scss/partials/variables.scss" as *;
+
+.header-wrapper {
+    width: 90%;
+}
+
+.navbar-brand img {
+    width: 8rem;
+}
+
+.nav-item {
+    text-transform: uppercase;
+}
+
+.nav-item .active {
+    color: $avd-primary;
+}
+
+.yellow-btn {
+    display: flex;
+
+}
+
+.yellow-btn button {
+    border: none;
+    text-align: center;
+    background-color: $avd-primary;
+    color: #000000A6;
+    padding: 0 1rem;
+}
+</style>
