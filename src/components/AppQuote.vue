a18f62a37aa3d56622063d2801b458ca55027f9e
davide-rullo <davide.rullo01@gmail.com>
2023-10-04 18:16:11 +0200

    add quote user

--- src/components/AppQuote.vue
+++ src/components/AppQuote.vue
@@ -1,6 +1,11 @@
 <script>
 export default {
-    name: 'AppQuote'
+    name: 'AppQuote',
+    data() {
+        return {
+
+        }
+    },
 }
 </script>
 <template>
@@ -10,6 +15,16 @@ export default {
                 <p>Our Home Owners Say</p>
                 <div class="hrow">
                 </div>
+            </div>
+            <div class="customer d-flex justify-content-center flex-column align-items-center gap-4 pt-4">
+
+
+                <img src="../assets/img/home-testimonial-113165296.jpg" alt="">
+                <p><i>“No man but feels more of a man in the world if he have but a bit of ground that he can call his own.
+                        However small it is on the surface, it is four thousand miles deep; and that is a very handsome
+                        property.”</i></p>
+                <h6 style="font-weight: 700;">HARRY SMITH, NEW HOME OWNER</h6>
+
             </div>
 
         </div>
@@ -34,5 +49,19 @@ export default {
 .quote-row p {
     font-size: 1.5rem;
     text-align: center;
+}
+
+
+
+img {
+    border-radius: 50%;
+    width: 200px;
+}
+
+.customer {
+    color: white;
+    text-align: center;
+    width: 60%;
+    margin: auto;
 }
 </style>
