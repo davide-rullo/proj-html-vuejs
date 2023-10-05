b50cf45b7c79e71ed846a803cfb6b616c39b830f
davide-rullo <davide.rullo01@gmail.com>
2023-10-05 16:02:01 +0200

    add carousel

--- src/components/AppQuote.vue
+++ src/components/AppQuote.vue
@@ -1,11 +1,53 @@
+
 <script>
+import image from '../assets/img/home-testimonial-113165296.jpg';
+import imageJohn from '../assets/img/home-testimonial-84268399.jpg';
+
 export default {
     name: 'AppQuote',
     data() {
         return {
-
+            activeImage: 0,
+            slides: [
+                {
+                    image: image,
+                    quote: 'No man but feels more of a man in the world if he have but a bit of ground that he can call his own.However small it is on the surface, it is four thousand miles deep; and that is a very handsomeproperty.',
+                    name: 'HARRY SMITH, NEW HOME OWNER'
+                }, {
+                    image: imageJohn,
+                    quote: 'Many novice real estate investors soon quit the profession and invest. When you invest in real estate, you often see a side of humanity that stocks, bonds, mutual funds, and saving money shelter you from.',
+                    name: 'JOHN DOE • PROPERTY INVESTOR'
+                },
+            ]
         }
     },
+
+    methods: {
+
+        next() {
+            this.activeImage++
+            if (this.activeImage > this.slides.length - 1) {
+                this.activeImage = 0
+            }
+        },
+
+        prev() {
+            this.activeImage--
+            if (this.activeImage < 0) {
+                this.activeImage = this.slides.length - 1
+            }
+        },
+
+        autoplay() {
+            this.auto = setInterval(this.next, 3000);
+        },
+    },
+
+    created() {
+        this.autoplay()
+
+    }
+
 }
 </script>
 <template>
@@ -19,11 +61,31 @@ export default {
             <div class="customer d-flex justify-content-center flex-column align-items-center gap-4 pt-4">
 
 
-                <img src="../assets/img/home-testimonial-113165296.jpg" alt="">
-                <p><i>“No man but feels more of a man in the world if he have but a bit of ground that he can call his own.
-                        However small it is on the surface, it is four thousand miles deep; and that is a very handsome
-                        property.”</i></p>
-                <h6 style="font-weight: 700;">HARRY SMITH, NEW HOME OWNER</h6>
+                <img :src="slides[activeImage].image" alt="">
+                <p><i>“{{ slides[activeImage].quote }}”</i></p>
+                <h6 style="font-weight: 700;">{{ slides[activeImage].name }}</h6>
+                <div class="controls d-flex gap-3">
+                    <svg v-if="activeImage === 0" xmlns="http://www.w3.org/2000/svg" @click="prev()" height="1em"
+                        viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" @click="next()" height="1em"
+                        viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path
+                            d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" />
+                    </svg>
+
+
+                    <svg v-if="activeImage === 0" xmlns="http://www.w3.org/2000/svg" @click="next()" height="1em"
+                        viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path
+                            d="M464 256A208 208 0 1 0 48 256a208 208 0 1 0 416 0zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256z" />
+                    </svg>
+                    <svg v-else xmlns="http://www.w3.org/2000/svg" @click="prev()" height="1em"
+                        viewBox="0 0 512 512"><!--! Font Awesome Free 6.4.2 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. -->
+                        <path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512z" />
+                    </svg>
+                </div>
 
             </div>
 
